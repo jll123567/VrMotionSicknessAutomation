35c25b73cb86f8bf52d9fdbb679b99273e520ab6
Jacob Ledbetter <ledbetterj1@wit.edu>
2024-02-20 15:49:39 -0500

    Working on making the numeric model.

--- motionsicknesspredict.py
+++ motionsicknesspredict.py
@@ -185,6 +185,27 @@ def load_dataset():
     pass
 
 
+def make_numeric_model(input_shape):
+    input_layer = keras.layers.Input(input_shape)
+
+    conv1 = keras.layers.Conv2D(filters=64, kernel_size=3, padding="same")(input_layer)
+    conv1 = keras.layers.BatchNormalization()(conv1)
+    conv1 = keras.layers.ReLU()(conv1)
+
+    conv2 = keras.layers.Conv2D(filters=64, kernel_size=3, padding="same")(conv1)
+    conv2 = keras.layers.BatchNormalization()(conv2)
+    conv2 = keras.layers.ReLU()(conv2)
+
+    conv3 = keras.layers.Conv2D(filters=64, kernel_size=3, padding="same")(conv2)
+    conv3 = keras.layers.BatchNormalization()(conv3)
+    conv3 = keras.layers.ReLU()(conv3)
+
+    gap = keras.layers.GlobalAveragePooling1D()(conv3)
+
+    output_layer = keras.layers.Dense(5, activation="softmax")(gap)  # units is 5 since there are five classes(rating 1-5)
+
+    return keras.model.Model(inputs=input_layer, output=output_layer)
+
 if __name__ == "__main__":
     images = load_images('/home/lambda8/ledbetterj1_VRMotionSickness/dataset/VRNetDataCollection/Pottery/P22 VRLOG-6061422')
     pass
