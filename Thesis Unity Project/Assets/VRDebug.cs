666b6ff6f2a50da238c1720dbd2ce4cd0dc8a845
JillTheSomething <ledbetterte@gmail.com>
2024-04-25 17:09:31 -0400

    Fix continuous turning and joystick debug.

--- Thesis Unity Project/Assets/VRDebug.cs
+++ Thesis Unity Project/Assets/VRDebug.cs
@@ -25,7 +25,7 @@ public class VRDebug : MonoBehaviour
     {
 
         CamDebug.text = $"Camera Data\n  Projection\n{PrintMatrix(sp.camdata.GetRange(0, 16), 4)}\n  View\n{PrintMatrix(sp.camdata.GetRange(16, 16), 4)}";
-        CtrlDebug.text = $"ControllerData\n  Joystick\n    Left: ({sp.ctrldata[0]}, {sp.ctrldata[1]})\n    Right: ({sp.ctrldata[2]}, {sp.ctrldata[3]})\n  Touchpad\n    Left: ({sp.ctrldata[4]}, {sp.ctrldata[5]})\n    Right: ({sp.ctrldata[6]}, {sp.ctrldata[7]})\n  Trigger\n    Left: {sp.ctrldata[8]}\n    Right: {sp.ctrldata[9]}";
+        CtrlDebug.text = $"ControllerData\n  Joystick\n    Left: ({sp.ctrldata[12]}, {sp.ctrldata[14]})\n    Right: ({sp.ctrldata[22]}, {sp.ctrldata[24]})";
         PoseDebug.text = $"PoseData\n  Hmd\n    deviceToAbsoluteTracking\n{PrintMatrix(sp.posedata.GetRange(0, 12), 6)}\n    Velocity: ({sp.posedata[12]}, {sp.posedata[13]}, {sp.posedata[14]})\n    AngularVelocity: ({sp.posedata[15]}, {sp.posedata[16]}, {sp.posedata[17]})\n  Left Controller\n    deviceToAbsoluteTracking\n{PrintMatrix(sp.posedata.GetRange(18, 12), 6)}\n    Velocity: ({sp.posedata[30]}, {sp.posedata[31]}, {sp.posedata[32]})\n    AngularVelocity: ({sp.posedata[33]}, {sp.posedata[34]}, {sp.posedata[35]})\n  Right Controller\n    deviceToAbsoluteTracking\n{PrintMatrix(sp.posedata.GetRange(36, 12), 6)}\n    Velocity: ({sp.posedata[48]}, {sp.posedata[49]}, {sp.posedata[50]})\n    AngularVelocity: ({sp.posedata[51]}, {sp.posedata[52]}, {sp.posedata[53]})";
         ImageDebug.texture = sp.headsetImage;
     }
